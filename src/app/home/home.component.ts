190aebc42fc647941058e95304a29b557cf5741a
unknown <YY140@ccs.local>
2024-05-15 12:00:34 +0800

    dynamic event list ok

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { AccountService } from '@app/_services';
+import { AccountService, EventService, AlertService } from '@app/_services';
 
 @Component({
   templateUrl: 'home.component.html'
@@ -8,7 +8,23 @@ import { AccountService } from '@app/_services';
 export class HomeComponent {
   account = this.accountService.accountValue;
 
-  constructor(private accountService: AccountService, private router: Router) { }
+  events: any[] = [];
+
+  constructor(private accountService: AccountService, private router: Router, private eventService: EventService, private alertService: AlertService) { }
+
+  ngOnInit() {
+    this.loadEvents();
+  }
+
+  loadEvents() {
+    this.eventService.getAllEvents()
+      .subscribe({
+        next: events => this.events = events,
+        error: error => this.alertService.error(error)
+      });
+  }
+
+
 
   // Method to handle the event when the "Join" button is clicked
   joinEvent() {
