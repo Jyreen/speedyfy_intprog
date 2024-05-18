74b29663a29bbc8d2d38e7a98ff35b6c8c98b35e
unknown <teeherajy@gmail.com>
2024-05-19 00:51:25 +0800

    search ok

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,33 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AccountService, EventService, AlertService } from '@app/_services';
-
 
 @Component({
   templateUrl: 'home.component.html'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   account = this.accountService.accountValue;
-
   events: any[] = [];
+  filteredEvents: any[] = [];
+  searchQuery: string = '';
 
-  constructor(private accountService: AccountService, private router: Router, private eventService: EventService, private alertService: AlertService) { }
+  constructor(
+    private accountService: AccountService, 
+    private router: Router, 
+    private eventService: EventService, 
+    private alertService: AlertService
+  ) { }
 
   ngOnInit() {
     this.loadEvents();
   }
 
   loadEvents() {
-    this.eventService.getAllEvents()
-      .subscribe({
-        next: events => this.events = events,
-        error: error => this.alertService.error(error)
-      });
+    this.eventService.getAllEvents().subscribe({
+      next: events => {
+        this.events = events;
+        this.filteredEvents = events; // Initialize filtered events
+      },
+      error: error => this.alertService.error(error)
+    });
   }
 
-  // Method to handle the event when the "Join" button is clicked
-  joinEvent() {
-    console.log('Join button clicked');
-    this.router.navigate(['/event-registration']);
+  filterEvents() {
+    const query = this.searchQuery.toLowerCase();
+
+    if (!query) {
+      this.filteredEvents = this.events;
+    } else {
+      this.filteredEvents = this.events.filter(event =>
+        event.name.toLowerCase().includes(query) ||
+        event.description.toLowerCase().includes(query) ||
+        event.location.toLowerCase().includes(query)
+      );
+    }
+  }
+
+  joinEvent(eventId: string) {
+    this.router.navigate(['/event-registration', eventId]);
   }
 }
