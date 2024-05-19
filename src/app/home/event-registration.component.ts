47113048d99bdeebf3aab9d8497ed45b36b705f3
unknown <teeherajy@gmail.com>
2024-05-20 00:30:27 +0800

    improved UI sa add-event.component.ts

--- src/app/home/event-registration.component.ts
+++ src/app/home/event-registration.component.ts
@@ -2,6 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AccountService, EventService, AlertService } from '@app/_services';
 
+
+import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
+
+
 @Component({
   selector: 'app-event-registration',
   templateUrl: './event-registration.component.html',
@@ -21,7 +28,8 @@ export class EventRegistrationComponent implements OnInit {
     private accountService: AccountService,
     private eventService: EventService,
     private alertService: AlertService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private http: HttpClient // Add this line
   ) { }
 
   ngOnInit() {
@@ -48,10 +56,36 @@ export class EventRegistrationComponent implements OnInit {
     this.loading = true;
     if (this.formData.terms) {
       console.log('Form Submitted', this.formData);
-      this.loading = false;
+  
+      // Send form data to the backend for validation
+      this.http.post<any>('http://localhost:4000/validate', this.formData)
+        .pipe(
+          catchError(error => {
+            this.alertService.error('An error occurred during form submission.');
+            return throwError(error);
+          })
+        )
+        .subscribe(response => {
+          // Assuming the backend responds with validation success and user email
+          const userEmail = response.email;
+  
+          // Send email to the user
+          this.http.post<any>('http://localhost:4000/send-registration-confirmation', { email: userEmail })
+            .pipe(
+              catchError(error => {
+                this.alertService.error('An error occurred while sending the email.');
+                return throwError(error);
+              })
+            )
+            .subscribe(() => {
+              this.alertService.success('Registration successful. Email sent.');
+              this.loading = false;
+            });
+        });
     } else {
       this.alertService.error('You must accept the terms and conditions.');
       this.loading = false;
     }
   }
+  
 }
